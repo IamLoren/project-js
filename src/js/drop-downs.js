6fddb76bc244f07ce5551c90ce7711fcc47c81e5
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-11-22 16:39:25 +0200

    fixed animation of buttons

--- src/js/drop-downs.js
+++ src/js/drop-downs.js
@@ -1,9 +1,29 @@
 export function openDropDown(event) {
-    event.target.closest
-    event.target.nextElementSibling.classList.toggle('list-active');
+const parentElement = this.closest('.filters-wrap');
+const svgElement = parentElement.querySelector('.filters-down-svg');
+const list = parentElement.querySelector('ul');
+
+    if (list.classList.contains('list-active')) {
+        list.classList.remove('list-active');
+    } else {
+        list.classList.add('list-active');
+    }
+   if (svgElement.classList.contains('rotate')) {
+    svgElement.classList.remove('rotate');
+   } else {
+    svgElement.classList.add('rotate');
+   }
 }
 
 export function rotateButton(event) {
-   this.classList.toggle('rotate');
-   this.previousElementSibling.classList.toggle('list-active');
+    if (this.classList.contains('rotate')) {
+        this.classList.remove('rotate');
+    } else {
+        this.classList.add('rotate');
+    }
+   if (this.previousElementSibling.classList.contains('list-active')) {
+    this.previousElementSibling.classList.remove('list-active');
+   } else {
+    this.previousElementSibling.classList.add('list-active');
+   }
 }
