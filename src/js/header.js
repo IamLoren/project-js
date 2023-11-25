e470a965fcf877c16cb091884811e0e8f0d2a031
Locomotive2te116 <locomotive2te116@gmail.com>
2023-11-25 21:46:19 +0200

    headerFunc

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,12 @@
+import {arrProducts} from './index.js'
 export function getLength() {
-    const array = localStorage.getItem('array');
-    if (!array) {
-                document.querySelector('#header-length').innerHTML = 0;
-        return;
-    } 
-    document.querySelector('#header-length').innerHTML = array.length;  
+    document.querySelector('#header-length').innerHTML = arrProducts.length;
+    // const array = localStorage.getItem('array');
+    // if (!array) {
+    //             document.querySelector('#header-length').innerHTML = 0;
+    //     return;
+    // } 
+    // document.querySelector('#header-length').innerHTML = array.length;  
 }
+// import {getLength} from './header.js'
+// getLength();
