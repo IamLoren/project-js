68a77c6e14c8a9fde71ca5ed8337fb344cf71ccb
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-11-26 10:33:02 +0200

    bug-fix modal functions

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,3 +1,5 @@
+import pathToSvg from '../images/icons.svg';
+import { getLength } from './header.js';
 import {
   openDropDown,
   rotateButton,
@@ -11,6 +13,7 @@ import {
   getAllProducts,
   getDiscountProducts,
   getPopularProducts,
+  getProducttById
 } from './api.js';
 import { renderMarkup } from './templates/cards.js';
 import { openProductModal } from './card-button.js';
@@ -135,3 +138,62 @@ searchForm.addEventListener('submit', async event => {
     console.log(error);
   }
 });
+
+// ДОДАВАННЯ ТОВАРУ В КОРЗИНУ З МОДАЛЬНОГО ВІКНА
+
+export async function addToCartFromModal(event) {
+  const productData = {};
+  const textBtn = event.target.innerText;
+  const id = event.currentTarget.getAttribute('data-id');
+  const isInCart = arrProducts.some(product => product.id === id); 
+         
+        if (!isInCart) {
+          event.currentTarget.innerHTML = `Remove from <svg class="modal-btn-svg" width="18" height="18">
+            <use class="modal-icon-svg" href="${pathToSvg}#icon-shopping-cart"></use>
+            </svg>`;
+            try {
+                const product = await getProducttById(id);
+               console.log(product);
+                const { category,  size, _id, name, price, img  } = product;
+                productData.category = category;
+                productData.size = size;
+                productData.id = _id;
+                productData.name = name;
+                productData.price = price;
+                productData.img = img;
+               } catch (error) {
+                console.log(error);
+               }
+
+                const localStorage = window.localStorage;
+            
+                arrProducts.push(productData);
+               
+                localStorage.setItem("product", JSON.stringify(arrProducts));
+            
+                getLength();
+    
+            
+      
+        } 
+
+    if (isInCart) {
+       event.currentTarget.innerHTML = `Add to <svg class="modal-btn-svg" width="18" height="18">
+        <use class="modal-icon-svg" href="${pathToSvg}#icon-shopping-cart"></use>
+        </svg>`;
+        // Видалити продукт з arrProducts
+        const idCard = event.currentTarget.getAttribute('data-id');
+        arrProducts = arrProducts.filter(item => item.id !== idCard);
+      
+        // Оновити локальне сховище із оновленим масивом arrProducts
+        localStorage.setItem('product', JSON.stringify(arrProducts));
+      
+        // Оновити текст і значок кнопки
+       
+      
+        // Оновити лічильник довжини
+        getLength();
+    }
+ 
+} 
+
