d1892e0e6eca88b93d6afc6a2527385777ca4e30
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-11-26 21:55:43 +0200

    fix getLength

--- src/js/header.js
+++ src/js/header.js
@@ -3,13 +3,6 @@ import localStorageAPI from './localStorage.js';
 export function getLength() {
     const arrFromLS = localStorageAPI.load('product')
     document.querySelector('#header-length').innerHTML = arrFromLS;
-    // const array = localStorage.getItem('array');
-    // if (!array) {
-    //             document.querySelector('#header-length').innerHTML = 0;
-    //     return;
-    // } 
-    // document.querySelector('#header-length').innerHTML = array.length;  
 }
-// import {getLength} from './header.js'
-// getLength();
+
 
