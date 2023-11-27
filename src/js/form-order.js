98d201b1a5522de7fed02325bd548b6775fe30bc
krisskov13 <kristinakovalenko333@gmail.com>
2023-11-27 17:31:38 +0200

    fix

--- src/js/form-order.js
+++ src/js/form-order.js
@@ -1,7 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { order } from './api.js';
-import { load, remove, save } from './localStorage.js';
+import localStorageAPI from './localStorage.js';
 
 const input = document.querySelector('.datetime');
 
@@ -14,27 +14,52 @@ const options = {
 
 flatpickr(input, options);
 
+// const order = async (order) => {
+//         try {
+//           const response = await axios.post(`https://food-boutique.b.goit.study/api/orders`, order, {
+//             headers: {
+//               'content-type': 'application/json',
+//             },
+//           });
+//           return response.data;
+//         } catch (error) {
+//           console.error("Order error: ", error.message);
+//         }
+// };
+      
+
+   document.addEventListener('DOMContentLoaded', function () {
+     const orderButton = document.querySelector('.cart-order-info-btn');
+
+     orderButton.addEventListener('submit', function (event) {
+       event.preventDefault();
+
+       const name = document.getElementById('name').value;
+       const phone = document.getElementById('phone').value;
+       const address = document.getElementById('address').value;
+       const datetime = document.getElementById('datetime').value;
+
+       const order = {
+         name,
+         phone,
+         address,
+         datetime,
+       };
+
+         console.log(order);
+       localStorageAPI.save('lastOrder', order);
+     });
+   });
+      
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const orderButton = document.querySelector('.cart-order-info-btn');
+// const orderButton = document.querySelector('.cart-order-info-btn');
+// const modale = document.querySelector('.order-backdrop');
+// const isHidden = document.querySelector('.is-hidden');
+// isHidden.style.display = 'none';
 
-    orderButton.addEventListener('submit', function (event) {
-        event.preventDefault();
+// orderButton.addEventListener('click', () => {
+//     modale.classList.toggle('is-hidden');
+// });
 
-        const name = document.getElementById('name').value;
-        const phone = document.getElementById('phone').value;
-        const address = document.getElementById('address').value;
-        const datetime = document.getElementById('datetime').value;
-
-        const order = {
-            name,
-            phone,
-            address,
-            datetime,
-        };
-
-        save('lastOrder', order);
-  });
-});
